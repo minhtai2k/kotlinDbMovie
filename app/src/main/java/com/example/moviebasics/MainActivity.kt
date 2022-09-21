48330d057ad6fb6afc96f02ef25124252c8c5a88
neo <vominhtaiks147@gmail.com>
2022-09-21 18:08:28 +0700

    commit module to test branch

--- app/src/main/java/com/example/moviebasics/MainActivity.kt
+++ app/src/main/java/com/example/moviebasics/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(){
     private lateinit var navController: NavController
